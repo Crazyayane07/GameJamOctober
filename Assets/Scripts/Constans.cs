7cc9f2c095adbcec9c3789a86037d4b70d300a11
Crazyayane07 <crazyayane07@gmail.com>
2020-10-24 11:38:29 +0200

    added main menu

---
+++ Assets/Scripts/Constans.cs
@@ -0,0 +1,9 @@
+﻿
+namespace OGJ
+{
+    public class Constans
+    {
+        public const string GAME_SCENE = "GameScene";
+        public const string MAIN_MENU_SCENE = "MainMenuScene";
+    }
+}
---
+++ Assets/Scripts/View/UI/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace OGJ.View.UI
+{
+    public class MainMenu : CustomMonoBehaviour
+    {
+        public Button startBtn;
+        public Button optionsBtn;
+        public Button exitBtn;
+
+        private void Awake()
+        {
+            SetUpButtons();
+        }
+        private void SetUpButtons()
+        {
+            startBtn.onClick.AddListener(StartGame);
+            optionsBtn.onClick.AddListener(OpenOptions);
+            exitBtn.onClick.AddListener(ExitGame);
+        }
+
+        private void StartGame()
+        {
+            SceneManager.LoadScene(Constans.GAME_SCENE);
+        }
+
+        private void OpenOptions()
+        {
+            //TO DO
+        }
+
+        private void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
