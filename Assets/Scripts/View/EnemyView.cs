a704ffc5a95f712597fbf138db7399b791cb5b2d
piotr <piotrekp94@gmail.com>
2020-10-24 20:31:40 +0200

    Add dummy enemy

---
+++ Assets/Scripts/View/EnemyView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("COLLISION");
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+}
