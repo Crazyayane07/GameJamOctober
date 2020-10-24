911aaf2b2632a3d36e5a910f371e5466ce046773
piotr <piotrekp94@gmail.com>
2020-10-24 20:34:43 +0200

    Add dummy enemy

--- Assets/Scripts/View/EnemyView.cs
+++ Assets/Scripts/View/EnemyView.cs
@@ -18,7 +18,6 @@ public class EnemyView : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("COLLISION");
         Destroy(collision.gameObject);
         Destroy(gameObject);
     }
