6f1ff3dd161d3f107fc4fafe721e16b637165989
Crazyayane07 <crazyayane07@gmail.com>
2020-10-24 16:19:22 +0200

    + scrolling background

---
+++ Assets/Scripts/View/ScrollingBackground.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace OGJ.View
+{
+    public class ScrollingBackground : CustomMonoBehaviour
+    {
+        public float speed = 0.1f;
+        public Renderer renderer;
+
+        private void Update()
+        {
+            Vector2 offset = new Vector2(Time.time * speed, 0);
+
+            renderer.material.mainTextureOffset = offset;
+        }
+    }
+}
